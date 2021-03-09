3f404601645ffcd67a4b439c472a5fae1e3f912f
Dtsoy <dmitriytsoy123@mail.com>
2021-03-08 22:45:42 -0800

    methodAndProperties

---
+++ method and properties.js
@@ -0,0 +1,38 @@
+"use strict"
+
+
+const str = "tset";
+
+console.log(str. length); // колво символов в строке 
+
+const arr = [1, 2, 4];
+
+console.log(arr. length); // колво символов в массиве
+
+
+const str = "teSt";
+
+//console.log(str. toUpperCase());
+console.log(str. toLowerCase());
+
+const fruit = "some fruit";
+
+console.log(fruit.indexOf("fruit")); 
+
+const logg = "hello world";
+
+console.log(logg.slice(0, 5)); 
+
+console.log(logg.substring(0 , 5)); 
+
+console.log(logg.substr(6, 5)); 
+
+
+const num = 12.4
+
+console.log(Math.round(num)); 
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
